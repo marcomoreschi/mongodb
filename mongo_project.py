8b22c4513fb5003b309857036e8af2eb65104cde
marcomoreschi <marcomoreschi91@hotmail.it>
2020-04-30 09:48:43 +0000

    added delete record

--- mongo_project.py
+++ mongo_project.py
@@ -93,6 +93,27 @@ def edit_record():
         except:
             print("Error accessing the database")
 
+def delete_record():
+
+    doc = get_record()
+
+    if doc:
+        print("")
+        for k, v in doc.items():
+            if k != "_id":
+                print(k.capitalize() + ": " + v.capitalize())
+        
+        print("")
+        confirmation = input("Is this the document you want to delete?\nY or N > ")
+        print("")
+
+        if confirmation.lower() == 'y':
+            try:
+                coll.remove(doc)
+                print("Document deleted!")
+            except:
+                print("Document not deleted")
+                
 
 def main_loop():
     while True:
@@ -104,7 +125,7 @@ def main_loop():
         elif option == "3":
             edit_record()
         elif option == "4":
-            print("You have selected option 4")
+            delete_record()
         elif option == "5":
             conn.close()
             break
